25196318dab6ad90e8ee95159f45741351e8d48c
Konstantin Klyatskin <klyatskin@gmail.com>
2019-12-02 22:08:13 -0500

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Konstantin Klyatskin on 2019-12-02.
+//  Copyright © 2019 Konstantin Klyatskin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ test/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  test
+//
+//  Created by Konstantin Klyatskin on 2019-12-02.
+//  Copyright © 2019 Konstantin Klyatskin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
