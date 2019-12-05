965c46bce4f19d2300f9dcd28cf2e7424019cec4
Konstantin Klyatskin <klyatskin@gmail.com>
2019-12-05 13:58:06 -0500

    Minor fix for LoginAnimator class

--- test/Login/LoginAnimator.h
+++ test/Login/LoginAnimator.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface LoginAnimator <UIViewControllerTransitioningDelegate>: UIViewController
+@interface LoginAnimator <UIViewControllerTransitioningDelegate>: NSObject
 
 
 @end
